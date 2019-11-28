c938539329e3ef1c5fb134c9e8c8e9f70e676a0e
ongraphmobility <mobilitydev@ongraph.com>
2019-11-28 14:09:53 +0530

    commit

--- SuperCarsMajlis/Controllers/Register/Register/View/RegisterViewController.swift
+++ SuperCarsMajlis/Controllers/Register/Register/View/RegisterViewController.swift
@@ -27,7 +27,6 @@ class RegisterViewController: UIViewController {
      */
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(Constants.kAppDelegate.kdeviceFCMToken!)
         // Do any additional setup after loading the view.
     }
     
